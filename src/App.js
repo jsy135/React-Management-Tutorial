4236896d19d7da0633de146fbb795b63c8a8529c
정성용 <wjdtjddyd135@naver.com>
2020-09-15 13:23:29 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gray-background">
+        <img src={logo} alt="logo" />
+        <h2>Let's develop management system!</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
